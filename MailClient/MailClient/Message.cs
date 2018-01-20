cfcb9dc04bb8d46e514d05b7bbe40a286dcdc1d4
Egor Mischerin <e.mischerin@gmail.com>
2018-01-20 14:26:23 +0300

    Added Mail Message Class

---
+++ MailClient/MailClient/Message.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace MailClient
+{
+	[Serializable]
+	class Message
+	{
+		private MailMessage mailmessage = new MailMessage();
+
+		public MailAddress Sender
+		{
+			get { return mailmessage.From; }
+			set { mailmessage.From = value; }
+		}
+
+		public MailAddressCollection Destination
+		{
+			get { return mailmessage.To; }
+			set { mailmessage.To.Add(value.ToString()); }
+		}
+
+		public string Body
+		{
+			get { return mailmessage.Body; }
+			set { mailmessage.Body = value; }
+		}
+
+		public string Subject
+		{
+			get { return mailmessage.Subject; }
+			set { mailmessage.Subject = value; }
+		}
+
+		public bool SetHtml
+		{
+			get { return mailmessage.IsBodyHtml; }
+			set { mailmessage.IsBodyHtml = value; }
+		}
+
+	}
+}
